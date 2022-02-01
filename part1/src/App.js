09b9d9e14b1425642d46fafe9e9e8f01f391e560
loreliawand <loreliawand@gmail.com>
2022-02-01 10:46:42 +0100

    Results of lesson 'Introduction to React'

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const name = 'George';
+  const age = 40;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Lora" age={40 - 20} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
 
 export default App;
