c0bd7f8777bff87065d379b0b946f39c0655d45b
loreliawand <loreliawand@gmail.com>
2022-02-08 12:29:00 +0100

    'Structure of backend application, testing'

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['off', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['off', 'never'],
+    eqeqeq: 'off',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+  },
+};
--- models/note.js
+++ models/note.js
@@ -1,21 +1,15 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI;
-
-console.log('connected to', url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
-  content: { type: String, minLength: 5, required: true },
-  date: { type: Date, required: true },
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 });
 
